20a40a2657aa1abbcf62524b48e71929d42839f5
Antonio Eloy <101589161+antonioaraujozup@users.noreply.github.com>
2022-04-11 08:22:52 -0300

    Commit inicial

---
+++ src/main/java/br/com/zup/edu/revistas/api/RevistasApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.edu.revistas.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RevistasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RevistasApplication.class, args);
+	}
+
+}
